e945832009c0771e69b2c91d3c1157dcc2e0ea77
ajujacob <ajujacob88@gmail.com>
2024-04-02 12:15:02 +0530

    doing add student screen

--- lib/screens/add_students_screen.dart
+++ lib/screens/add_students_screen.dart
@@ -1,13 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class AddStudentsScreen extends StatelessWidget {
-  AddStudentsScreen({super.key});
+class AddStudentsScreen extends StatefulWidget {
+  const AddStudentsScreen({super.key});
 
+  @override
+  State<AddStudentsScreen> createState() => _AddStudentsScreenState();
+}
+
+class _AddStudentsScreenState extends State<AddStudentsScreen> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController placeController = TextEditingController();
   final TextEditingController dobController = TextEditingController();
   final TextEditingController ageController = TextEditingController();
   String? selectedGender;
+
+  late DateTime selectedDate;
+  int age = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedDate = DateTime.now();
+  }
+
+  void _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+
+    if (picked != null) {
+      setState(() {
+        //dobController.text = picked.toString(); // Display selected date
+        dobController.text = DateFormat('dd-MM-yyyy')
+            .format(picked); // Display selected date in dd-MM-yyyy format
+      });
+    }
+
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+        // Calculate age
+        final now = DateTime.now();
+        age = now.year -
+            picked.year -
+            ((now.month > picked.month ||
+                    (now.month == picked.month && now.day >= picked.day))
+                ? 0
+                : 1);
+
+        ageController.text = age.toString();
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,13 +88,49 @@ class AddStudentsScreen extends StatelessWidget {
               height: 16,
             ),
             DropdownButtonFormField(
+                hint: const Text('Select Gender'),
                 items: ['Male', 'Female', 'Other']
                     .map((gender) =>
                         DropdownMenuItem(value: gender, child: Text(gender)))
                     .toList(),
                 onChanged: (value) {
                   selectedGender = value.toString();
-                })
+                }),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () => _selectDate(context),
+              child: const Text('Select Date of Birth'),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            //  Text(                'Date of Birth: ${DateFormat('dd/MM/yyyy').format(selectedDate)}'),
+            const SizedBox(
+              height: 16,
+            ),
+            Text('Age: $age'),
+            TextFormField(
+              initialValue: '$age',
+            ),
+            TextFormField(
+              controller: dobController,
+              decoration: InputDecoration(
+                labelText: 'Date of Birth',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.calendar_today),
+                  onPressed: () => _selectDate(context),
+                ),
+              ),
+            ),
+            TextFormField(
+              readOnly: true,
+              controller: ageController,
+              decoration: const InputDecoration(
+                labelText: 'Age',
+              ),
+            ),
           ],
         ),
       ),
