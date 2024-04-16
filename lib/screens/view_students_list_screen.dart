e9feb6c379050734bd259fca2a18ad2b25539ce9
ajujacob <ajujacob88@gmail.com>
2024-04-16 21:37:02 +0530

    search students build actions and buildresults completed

--- lib/screens/view_students_list_screen.dart
+++ lib/screens/view_students_list_screen.dart
@@ -170,6 +170,12 @@ class MyCustomSearchDelegate extends SearchDelegate<List<Student>> {
             student.name!.toLowerCase().contains(query.toLowerCase()))
         .toList();
 
+    if (filteredStudents.isEmpty) {
+      return Center(
+        child: Text('No students found for "$query"'),
+      );
+    }
+
     return ListView.separated(
       separatorBuilder: (context, index) {
         return const Divider();
@@ -226,120 +232,71 @@ class MyCustomSearchDelegate extends SearchDelegate<List<Student>> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    // Implement logic to display suggestions based on search text (optional)
-    return const Center(
-      child: Text('Search suggestions'),
-    );
-  }
-}
+    // // Implement logic to display suggestions based on search text (optional)
+    // return const Center(
+    //   child: Text('Search suggestions'),
+    // );
 
+    final suggestedStudents = query.isEmpty
+        ? []
+        : studentsList
+            .where((student) =>
+                student.name!.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
 
-
-
-
-
-
-/*
-import 'package:flutter/material.dart';
-import 'package:sample_student_record_using_sqflite/models/student_data.dart';
-import 'package:sample_student_record_using_sqflite/screens/view_students_details_screen.dart';
-import 'package:sample_student_record_using_sqflite/db/database_helper.dart';
-
-class ViewStudentsListScreen extends StatelessWidget {
-  ViewStudentsListScreen({super.key, required this.studentsList});
-
-  final List<Student> studentsList;
-
-  // final DatabaseHelperr _databaseHelper = DatabaseHelperr();
-  // Future<List<Student>> _getStudents() async {
-  //   final students = await _databaseHelper.getAllStudents();
-  //   return students.map((student) => Student.fromMap(student)).toList();
-  // }
-
-  // Future<void> _getStudents() async {
-  //   final students = await _databaseHelper.getAllStudents();
-  //   var studentsList2 =
-  //       students.map((student) => Student.fromMap(student)).toList();
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    //print('st list is $studentsList ');
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Students List'),
-      ),
-      body: ListView.separated(
-        separatorBuilder: (context, index) {
-          return const Divider();
-        },
-        itemCount: studentsList.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-              title: Text(studentsList[index].name!),
-              subtitle: Text(studentsList[index].place!),
-              // leading: Image.memory(studentsList[index].profilePic!),
-              // leading: studentsList[index].profilePic != null
-              //     ? Image.memory(studentsList[index].profilePic!)
-              //     : const Icon(Icons
-              //         .account_circle), // Placeholder icon when profile pic is null
-
-              // leading: studentsList[index].profilePic != null
-              //     ? CircleAvatar(
-              //         backgroundImage:
-              //             MemoryImage(studentsList[index].profilePic!),
-              //         radius: 25, // Adjust the radius as needed
-              //       )
-              //     : const CircleAvatar(
-              //         child: const Icon(Icons.account_circle_rounded),
-              //         radius: 25, // Adjust the radius as needed
-              //       ),
-
-              leading: studentsList[index].profilePic != null
-                  ? GestureDetector(
-                      onTap: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return Dialog(
-                              child: Container(
-                                width: MediaQuery.of(context).size.width,
-                                height: MediaQuery.of(context).size.width,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: MemoryImage(
-                                        studentsList[index].profilePic!),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
+    return ListView.separated(
+      separatorBuilder: (context, index) {
+        return const Divider();
+      },
+      itemCount: suggestedStudents.length,
+      itemBuilder: (context, index) {
+        // final student = suggestedStudents[index];
+        return ListTile(
+          title: Text(suggestedStudents[index].name!),
+          subtitle: Text(suggestedStudents[index].place!),
+          leading: suggestedStudents[index].profilePic != null
+              ? GestureDetector(
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Dialog(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: MemoryImage(
+                                    suggestedStudents[index].profilePic!),
+                                fit: BoxFit.cover,
                               ),
-                            );
-                          },
+                            ),
+                          ),
                         );
                       },
-                      child: CircleAvatar(
-                          radius: 25,
-                          backgroundImage:
-                              MemoryImage(studentsList[index].profilePic!)),
-                    )
-                  : const CircleAvatar(
+                    );
+                  },
+                  child: CircleAvatar(
                       radius: 25,
-                      child: Icon(Icons.account_circle_rounded),
-                      // Adjust the radius as needed
-                    ),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: ((context) => ViewStudentsDetailsScreen(
-                          studentDetail: studentsList[index],
-                        )),
-                  ),
-                );
-              });
-        },
-      ),
+                      backgroundImage:
+                          MemoryImage(suggestedStudents[index].profilePic!)),
+                )
+              : const CircleAvatar(
+                  radius: 25,
+                  child: Icon(Icons.account_circle_rounded),
+                  // Adjust the radius as needed
+                ),
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: ((context) => ViewStudentsDetailsScreen(
+                      studentDetail: suggestedStudents[index],
+                    )),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
-*/
